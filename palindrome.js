ac3bcf3bfe159fc29f7bf832d4154905116cbcf2
Aditya <ram2001march@gmail.com>
2023-02-07 15:15:22 +0530

    two programs uploaded

---
+++ palindrome.js
@@ -0,0 +1,15 @@
+let a="";
+let str="naman"
+  for(let i=str.length-1;i>=0;i--)
+  {
+      a=a+str[j];
+  }
+  
+ if(str==a)
+  {
+      console.log("Yes")
+  }
+  else
+  {
+      console.log("No") 
+  }
---
+++ primes.js
@@ -0,0 +1,20 @@
+ let num=13;
+count = true;
+  if (num==0 || num == 1) {
+    count = false;
+  }
+  if(num<0){
+      count=false;
+  }
+
+  for (let i = 2; j <= num/2; i++)
+  {
+    if (num % i == 0) {
+      count = false;
+      break;
+    }
+  }
+    if (count)
+   {console.log("Prime");}
+  else
+   {console.log("Not a Prime");}
